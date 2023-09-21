25c6cf7604c361645f6fc4c172d870a0941bc66a
kaio <iceythe@gmail.com>
2023-09-21 20:17:50 +0200

    Cleanup: - Code style - Add annotations - Favor comments over docstrings - Use lookup indirection to minimize polluting module

--- plugins/undo.py
+++ plugins/undo.py
@@ -6,24 +6,19 @@ This affects the operators:
 - ED_OT_undo_history: Resync when undoing a specific undo step.
 """
 
-from textension.utils import LinearStack, Adapter, TextOperator, text_from_id, iter_spaces, consume, map_not, filtertrue
-from textension.overrides import override, restore_capsules, _get_wmOperatorType
-from textension.overrides.default import Default
+from textension.btypes import defs
+from textension.utils import text_from_id, iter_spaces, consume, map_not
 from textension.core import ensure_cursor_view
-
-from textension.btypes.defs import OPERATOR_CANCELLED, OPERATOR_FINISHED, OPTYPE_UNDO
-from textension import utils
-
-from functools import partial
+from textension import utils, overrides
 from itertools import compress
-from operator import attrgetter, methodcaller
-from typing import Callable
 
 import bpy
 
-_context = utils._context
 
-undo_stacks: dict[int, LinearStack] = {}
+_context = utils._context
+_data    = utils._data
+
+undo_stacks: dict[int, utils.LinearStack] = {}
 
 
 # Dict of python operators using the new undo system
@@ -33,10 +28,13 @@ _pyop_store: dict[bpy.types.Operator, list[str]] = {}
 _applied_default_undos = []
 
 
-def _should_split(text: bpy.types.Text):
-    """Examines the last type character and determine if an undo that would
-    otherwise be grouped, should be split.
-    """
+_stack_sync_ids = set()
+_stack_sync_map = {}
+
+
+# Examines the last type character and determine if an undo that would
+# otherwise be grouped, should be split.
+def _should_split(text: bpy.types.Text) -> bool:
     # If the cursor has a selection, treat it as a split.
     curl, curc, sell, selc = text.cursor_sorted
     if curc != selc or curl != sell or curc < 1:
@@ -45,10 +43,10 @@ def _should_split(text: bpy.types.Text):
     return text.lines[curl].body[curc - 1] in separators
 
 
-class TextAdapter(Adapter):
+class TextAdapter(utils.Adapter):
     metadata: tuple[str, float, int]
 
-    def __init__(self, text: bpy.types.Text):
+    def __init__(self, text: bpy.types.Text) -> None:
         utils._check_type(text, bpy.types.Text)
         self.name = text.name
         self.id   = text.id
@@ -56,7 +54,7 @@ class TextAdapter(Adapter):
 
     @property
     def text(self) -> bpy.types.Text:
-        texts = bpy.data.texts
+        texts = _data.texts
         try:
             text = texts[self.name]
             text_id = text.id
@@ -93,13 +91,13 @@ class TextAdapter(Adapter):
         self.text.cursor = cursor
 
     @utils.inline
-    def set_string(self, string: str):
+    def set_string(self, string: str) -> None:
         return utils._forwarder("text.from_string")
 
-    def get_should_split(self, hint: bool):
+    def get_should_split(self, hint: bool) -> bool:
         return _should_split(self.text)
 
-    def on_update(self, restore=False):
+    def on_update(self, restore=False) -> None:
         text = self.text
         internal = text.internal
         if restore:
@@ -113,8 +111,8 @@ class TextAdapter(Adapter):
 
 
 # Dummy stack. Makes it easier to write generic code for undo/redo.
-@utils.inline_class(Adapter())
-class NO_STACK(LinearStack):
+@utils.inline_class(utils.Adapter())
+class NO_STACK(utils.LinearStack):
     undo = ()
     redo = ()
 
@@ -125,50 +123,53 @@ class NO_STACK(LinearStack):
         return utils.noop_noargs
 
 
-id_from_text  = attrgetter("id")
-sync_ids = set()
-sync_map = {}
+def get_undo_stack(text: bpy.types.Text) -> utils.LinearStack:
+    assert text.__class__ is bpy.types.Text
+    try:
+        return undo_stacks[text.id]
+    except KeyError:
+        return undo_stacks.setdefault(text.id, utils.LinearStack(TextAdapter(text)))
 
 
 @utils.inline
-def update_cursors(stacks: list[LinearStack]):
-    return methodcaller("update_cursor")
-
+def update_cursors(stacks: list[utils.LinearStack]):
+    return utils.methodcaller("update_cursor")
 
 @utils.inline
 def map_synced_ids(ids):
-    return partial(map, sync_ids.__contains__)
-
+    return utils.partial_map(_stack_sync_ids.__contains__)
 
 @utils.inline
 def map_ids_from_texts(texts):
-    return partial(map, id_from_text)
+    return utils.partial_map(utils.attrgetter("id"))
+
+@utils.inline
+def map_undo_stacks(texts):
+    return utils.partial_map(get_undo_stack)
+
+@utils.inline
+def map_cursor_updates(stacks):
+    return utils.partial_map(update_cursors)
 
 
-# The act of creating or removing texts should not be an undoable operation.
-# That's not how text editors work. So this attempts to fix that.
+# Called before undo/redo/undo_history.
+# This preserves the texts so they can be recovered.
 @utils.unsuppress
-def sync_pre():
-    """Called before undo/redo/undo_history.
-    This preserves the texts so they can be recovered.
-    """
-    stacks = list(map(get_undo_stack, bpy.data.texts))
-    consume(map(update_cursors, stacks))
-    sync_ids.update(map_ids_from_texts(bpy.data.texts))
+def sync_pre() -> None:
+    texts  = _data.texts
+    consume(map_cursor_updates(map_undo_stacks(texts)))
+    _stack_sync_ids.update(map_ids_from_texts(texts))
 
     for st in iter_spaces(space_type='TEXT_EDITOR'):
-        if text := st.text:
-            sync_map[st] = id_from_text(text)
-        else:
-            sync_map[st] = None
+        _stack_sync_map[st] = getattr(st.text, "id", None)
 
 
+# Called after undo/redo/undo_history.
 @utils.unsuppress
-def sync_post():
-    """Called after undo/redo/undo_history."""
+def sync_post() -> None:
     # Remove texts whose ids dont match those from sync_pre.
-    saved_ids = map_synced_ids(map_ids_from_texts(bpy.data.texts))
-    bpy.data.batch_remove(compress(bpy.data.texts, map_not(saved_ids)))
+    saved_ids = map_synced_ids(map_ids_from_texts(_data.texts))
+    _data.batch_remove(compress(_data.texts, map_not(saved_ids)))
 
     # Restore texts Blender removed from its undo/redo step.
     for stack in tuple(undo_stacks.values()):
@@ -176,91 +177,82 @@ def sync_post():
 
     # Restore the assigned texts to open editors.
     for st in iter_spaces(space_type='TEXT_EDITOR'):
-        st.text = text_from_id(sync_map.get(st))
+        st.text = text_from_id(_stack_sync_map.get(st))
 
-    sync_map.clear()
-    sync_ids.clear()
+    _stack_sync_map.clear()
+    _stack_sync_ids.clear()
 
 
+# Purge stacks and create new for any text blocks.
+# This is called via bpy.app.handlers.load_post.
 @bpy.app.handlers.persistent
-def purge(*unused_args):
-    """Purge stacks and create new for any text blocks.
-    This is called via bpy.app.handlers.load_post.
-    """
+def purge(*unused_args) -> None:
     for stack in undo_stacks.values():
         stack.undo.clear()
         stack.redo.clear()
 
     undo_stacks.clear()
-    consume(map(get_undo_stack, bpy.data.texts))
-
-
-def _has_undo(cls: bpy.types.Operator):
-    return 'UNDO' in getattr(cls, "bl_options", set())
+    consume(map_undo_stacks(_data.texts))
 
 
 # Method wrapper for python operators. Similar to the C-method wrapper below.
-def pyop_wrapper(context, op=None, event=None, method: Callable = None):
+def pyop_wrapper(context, op=None, event=None, method=None) -> int:
     # Ensure a stack exists before calling any operators.
     stack = get_active_stack()
-    result = method(*filtertrue((context, op, event)))
+    try:
+        result = method(*utils.filtertrue((context, op, event)))
+    except:
+        import traceback
+        traceback.print_exc()
+        return defs.OPERATOR_CANCELLED
 
-    if result == OPERATOR_FINISHED:
+    if result == defs.OPERATOR_FINISHED:
         stack.push_undo(tag=op.contents.idname.decode())
         # Prevent Blender from pushing its own undo.
-        result = OPERATOR_CANCELLED
+        result = defs.OPERATOR_CANCELLED
     return result
 
 
-def pyop_enable_new_undo(cls) -> bool:
+def pyop_enable_new_undo(cls) -> None:
     utils._check_type(cls, bpy.types.Operator)
+
+    # TODO: We could support unregistered python operators by defining a
+    # ``register/unregister`` methods or wrap them if they already exist.
     assert cls.is_registered, f"Operator '{cls}' is not registered"
 
-    if not _has_undo(cls):
-        return
+    if 'UNDO' in getattr(cls, "bl_options", ()):
+        idname = cls.bl_rna.identifier
+        methods = _pyop_store.setdefault(cls, [])
 
-    idname = cls.bl_rna.identifier
-    methods = _pyop_store.setdefault(cls, [])
-
-    for method in ("invoke", "execute", "modal"):
-        if hasattr(cls, method):
-            if method == "execute":
-                method = "exec"
-            methods += override(idname, method, pyop_wrapper),
+        for method in ("invoke", "execute", "modal"):
+            if hasattr(cls, method):
+                if method == "execute":
+                    method = "exec"
+                methods += overrides.override(idname, method, pyop_wrapper),
 
 
-def pyop_disable_new_undo(cls):
-    if not _has_undo(cls):
-        assert cls not in _pyop_store
-        return
-    restore_capsules(_pyop_store[cls])
-    _pyop_store[cls].clear()
-    del _pyop_store[cls]
+def pyop_disable_new_undo(cls) -> None:
+    if cls in _pyop_store:
+        overrides.restore_capsules(_pyop_store[cls])
+        _pyop_store[cls].clear()
+        del _pyop_store[cls]
 
 
-def get_undo_stack(text: bpy.types.Text):
-    assert text.__class__ is bpy.types.Text
-    try:
-        return undo_stacks[text.id]
-    except KeyError:
-        return undo_stacks.setdefault(text.id, LinearStack(TextAdapter(text)))
-
-
-def get_active_stack():
+def get_active_stack() -> utils.LinearStack:
     if text := getattr(_context, "edit_text", None):
         return get_undo_stack(text)
     return NO_STACK
 
 
-def undo_poll():
+def undo_poll() -> bool:
     return get_active_stack().poll_undo()
 
 
-def redo_poll():
+def redo_poll() -> bool:
     return get_active_stack().poll_redo()
 
 
-def undo_pre():
+def undo_pre() -> bool:
     if stack := get_active_stack():
         if stack.pop_undo():
             ensure_cursor_view(speed=2)
@@ -268,7 +260,8 @@ def undo_pre():
     return False
 
 
-def redo_pre():
+# XXX: This is nearly identical to ``undo_pre``.
+def redo_pre() -> bool:
     if stack := get_active_stack():
         if stack.pop_redo():
             ensure_cursor_view(speed=2)
@@ -276,28 +269,29 @@ def redo_pre():
     return False
 
 
-def unlink_pre():
+def unlink_pre() -> None:
     undo_stacks.pop(_context.edit_text.id, None)
 
 
-def new_post(result):
-    if result == OPERATOR_FINISHED:
-        get_undo_stack(_context.text)
+def new_post(result) -> None:
+    if result == defs.OPERATOR_FINISHED:
+        get_undo_stack(_context.edit_text)
 
 
-def save_post(result=None):
-    if result in {None, OPERATOR_FINISHED}:
+def save_post(result=None) -> None:
+    if result in {None, defs.OPERATOR_FINISHED}:
         # Update filepath, modified time and flags from text.
         stack = get_undo_stack(_context.edit_text)
         stack.adapter.update()
 
 
 # This applies the new undo for all Default operator overrides.
-def _apply_default_undo():
+def _apply_default_undo() -> None:
+    from textension.overrides.default import Default
     for cls in Default.operators:
-        ot = _get_wmOperatorType(cls.__name__)
+        ot = overrides._get_wmOperatorType(cls.__name__)
 
-        if not ot.flag & OPTYPE_UNDO:
+        if not ot.flag & defs.OPTYPE_UNDO:
             continue
 
         for method in cls.get_defined_methods():
@@ -317,12 +311,12 @@ def _apply_default_undo():
                 try:
                     result = method(self)
                 except:
-                    result = OPERATOR_CANCELLED
+                    result = defs.OPERATOR_CANCELLED
                 else:
                     # Operator finished, push a step, ignore native undo.
-                    if result == OPERATOR_FINISHED:
+                    if result == defs.OPERATOR_FINISHED:
                         stack.push_undo(tag=cls.__name__)
-                        result = OPERATOR_CANCELLED
+                        result = defs.OPERATOR_CANCELLED
                 finally:
                     return result
 
@@ -332,20 +326,20 @@ def _apply_default_undo():
             _applied_default_undos.append((cls, name, method))
 
 
-def _remove_default_undo():
+def _remove_default_undo() -> None:
     for restore_args in _applied_default_undos:
         setattr(*restore_args)
     _applied_default_undos.clear()
 
 
-def enable():
+def enable() -> None:
     # Registered TextOperators.
-    for cls in TextOperator.__subclasses__():
+    for cls in utils.TextOperator.__subclasses__():
         if cls.is_registered:
             pyop_enable_new_undo(cls)
 
-    TextOperator._register_hooks += pyop_enable_new_undo,
-    TextOperator._unregister_hooks += pyop_disable_new_undo,
+    utils.TextOperator._register_hooks += pyop_enable_new_undo,
+    utils.TextOperator._unregister_hooks += pyop_disable_new_undo,
 
     # Internal operators.
     _apply_default_undo()
@@ -377,9 +371,9 @@ def enable():
     TEXT_OT_resolve_conflict.add_post(save_post, is_global=True)
 
 
-def disable():
-    TextOperator._register_hooks.remove(pyop_enable_new_undo)
-    TextOperator._unregister_hooks.remove(pyop_disable_new_undo)
+def disable() -> None:
+    utils.TextOperator._register_hooks.remove(pyop_enable_new_undo)
+    utils.TextOperator._unregister_hooks.remove(pyop_disable_new_undo)
 
     # Restore Python-based operators.
     for cls in list(_pyop_store):
